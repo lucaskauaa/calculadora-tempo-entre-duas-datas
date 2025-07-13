8f2df88995448d085ae5a0cac1dfc0ff2fd166fe
lucaskaua <lucaskauadev@gmail.com>
2025-07-12 21:59:58 -0300

    Finalizando funções para instânciar objetos de data e hora a partir da entrada do usuário e calcular o tempo entre eles

--- src/program/Main.java
+++ src/program/Main.java
@@ -2,41 +2,57 @@ package program;
 
 import java.util.Scanner;
 
+import util.DateTimeCalculator;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Insira dia, mês e ano no formato dd/mm/aaaa");
+		char repeat;
 		
-		System.out.println();
-		System.out.print("Data de inicio: ");
-		String startDate = sc.nextLine();
-				
-		System.out.print("Data final: ");
-		String endDate = sc.nextLine();
-		
-		System.out.print("Deseja incluir o horário? s/n ");
-		char hasTime = sc.next().charAt(0);
-		hasTime = Character.toLowerCase(hasTime);
-		
-		if (hasTime == 's') {
+		do {
+			System.out.println("Insira dia, mês e ano no formato dd/mm/aaaa");
+			
 			System.out.println();
-			System.out.println("Insira o horário no formato hh:mm");
+			System.out.print("Data de início: ");
+			String startDate = sc.nextLine();
+					
+			System.out.print("Data final: ");
+			String endDate = sc.nextLine();
 			
-			System.out.print("Horário da data de início: ");
-			sc.nextLine();
-			String startTime = sc.nextLine();
+			System.out.println();
+			System.out.print("Deseja incluir o horário? s/n ");
+			char hasTime = sc.next().charAt(0);
+			hasTime = Character.toLowerCase(hasTime);
 			
-			System.out.print("Horário da data final: ");
-			String endTime = sc.nextLine();
-			
-		} else {
-			
-		}
-
-		
+			if (hasTime == 's') {
+				System.out.println();
+				System.out.println("Insira o horário no formato hh:mm");
 				
+				System.out.println();
+				System.out.print("Horário da data de início: ");
+				sc.nextLine();
+				String startTime = sc.nextLine();
+				
+				System.out.print("Horário da data final: ");
+				String endTime = sc.nextLine();
+				
+				DateTimeCalculator.calculateDateTime(startDate, startTime, endDate, endTime);
+				
+			} else {
+				DateTimeCalculator.calculateDateTime(startDate, endDate);
+			}
+			
+			System.out.println();
+			System.out.print("Deseja repetir? s/n ");
+			repeat = sc.next().charAt(0);
+			repeat = Character.toLowerCase(repeat);
+			sc.nextLine();
+			System.out.println();
+			
+		} while (repeat == 's');
+		
 		sc.close();
 
 	}
--- src/util/DateTimeCalculator.java
+++ src/util/DateTimeCalculator.java
@@ -1,7 +1,9 @@
 package util;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeCalculator {
@@ -15,6 +17,8 @@ public class DateTimeCalculator {
 		LocalDate date01 = LocalDate.parse(startDate, dateTimeFormatter);
 		
 		LocalDate date02 = LocalDate.parse(endDate, dateTimeFormatter);
+		
+		calculate(date01.atStartOfDay(), date02.atStartOfDay());
 	}
 	
 	public static void calculateDateTime(String startDate, String startTime, String endDate, String endTime) {
@@ -28,10 +32,34 @@ public class DateTimeCalculator {
 		LocalDateTime dateTime01 = LocalDateTime.parse(startDate + " " + startTime, dateTimeFormatter);
 		
 		LocalDateTime dateTime02 = LocalDateTime.parse(endDate + " " + endTime, dateTimeFormatter);
+		
+		calculate(dateTime01, dateTime02);
 	}
 	
-//	public static void calculate (LocalDateTime startDateTime, LocalDateTime endDateTime) {
-//		
-//		
-//	}
+	private static void calculate (LocalDateTime startDateTime, LocalDateTime endDateTime) {
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println();
+		System.out.println("==============================================");
+		System.out.println("Data inicial: " + startDateTime.format(dateTimeFormatter));
+		System.out.println("Data final: " + endDateTime.format(dateTimeFormatter));
+		
+		LocalDate startDate = startDateTime.toLocalDate();
+		LocalDate endDate = endDateTime.toLocalDate();
+		
+		Period period = Period.between(startDate, endDate);
+		
+		System.out.println();
+		System.out.println("Tempo percorrido: " + period.getYears() + " anos, " + period.getMonths() + " meses e " + period.getDays() + " dias.");
+		
+		Duration duration = Duration.between(startDateTime, endDateTime);
+		
+		System.out.println();
+		System.out.println(duration.toHours() + " horas");
+		System.out.println(duration.toDays() + " dias");
+		System.out.println(duration.toDays() / 7 + " semanas");
+		System.out.println(duration.toDays() / 30 + " meses aproximadamente");
+		System.out.println(period.getYears() + " anos");
+		
+	}
 }
